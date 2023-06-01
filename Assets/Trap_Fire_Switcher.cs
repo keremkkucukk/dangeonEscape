caea43c93d330ea351adeeb45bc87e3b37b88107
keremkkucukk <keremkkucukk@gmail.com>
2023-06-01 11:56:38 +0300

    Dangeon Escape

---
+++ Assets/Trap_Fire_Switcher.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_Fire_Switcher : MonoBehaviour
+{
+    private Trap_Fire myTrap;
+    private Animator anim;
+
+
+    [SerializeField] private float timeNotActive = 2;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        myTrap = GetComponentInChildren<Trap_Fire>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            anim.SetTrigger("pressed");
+            myTrap.FireSwitchAfter(timeNotActive);
+        }
+    }
+}
